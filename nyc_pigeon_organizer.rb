1f3cca5466fed644121ff480b8640f6aea75b3ac
Alan sanchez <asanchezhaza@gmail.com>
2020-10-27 08:38:18 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ 
+ data.each_with_object({}) do |(key,values), final_array|
+  values.each do |inner_key,names|
+    names.each do |name|
+      if !final_array[name]
+        final_array[name] = {}
+ end
+   if !final_array[name][key] 
+     !final_array[name][key] = []
+   end
+   final_array[name][key].push(inner_key.to_s)
+    
+  end  
+end 
+end
+  
+  # i was not able to come up with my own solution on this one i will come back to it and try a different solution
 end
